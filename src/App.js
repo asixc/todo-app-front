f9690a5bcc16737054c5b3873381a7999ba01dce
Jose <joselitosbd@gmail.com>
2026-05-09 00:41:58 +0200

    Revert "fix(mobile): restore footer notepad style and original item sizing"
    This reverts commit 41fe958bff1623096cd8a677d10d32d51a7d9c80.

--- src/App.js
+++ src/App.js
@@ -225,7 +225,7 @@ function App() {
               <small
                 style={{ fontSize: "0.5em", marginLeft: "10px", color: "#888" }}
               >
-                v0.1.14
+                v0.1.13
               </small>
             </h1>
 
